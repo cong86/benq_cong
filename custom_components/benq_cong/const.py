ad27b6157cedb5897b0d3ac6a11fce4242420a24
cong86 <93272942+cong86@users.noreply.github.com>
2026-04-18 20:03:06 +0800

    Add files via upload

---
+++ custom_components/benq_cong/const.py
@@ -0,0 +1,15 @@
+"""Constants for the BenQ Projector integration."""
+
+from typing import Final
+
+DOMAIN: Final = "benqprojector"
+
+CONF_MODEL: Final = "model"
+
+CONF_TYPE_SERIAL: Final = "serial"
+CONF_TYPE_TELNET: Final = "telnet"
+
+CONF_SERIAL_PORT: Final = "serial_port"
+CONF_BAUD_RATE: Final = "baud_rate"
+CONF_INTERVAL: Final = "interval"
+CONF_DEFAULT_INTERVAL: Final = 5
